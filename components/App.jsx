86f0d0b5106ee83b00303bac62ef551efce28cc3
Jeremy Yochum <jyochum391@yahoo.com>
2024-04-03 13:11:25 -0400

    create fetch request to trivia api and pass data as props to TriviaPage.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react"
 import "../sass/styles.scss"
 import FrontPage from "./FrontPage"
+import TriviaPage from "./TriviaPage"
 
 export default function App() {
-    const [frontPageActive, setFrontPageActive] = useState(true)
+    const [frontPageActive, setFrontPageActive] = useState(false)
+    const [quizData, setQuizData] = useState([])
 
     function toggleFrontPage() {
         setFrontPageActive(previousValue => !previousValue)
     }
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5')
+            .then(res => res.json())
+            .then(data => setQuizData(data))
+            .catch(err => err.message)
+    }, [])
 
     return (
         <div>
@@ -17,6 +26,7 @@ export default function App() {
                 alt="yellow blob"
             />
             {frontPageActive && <FrontPage toggleFrontPage={toggleFrontPage} />}
+            <TriviaPage quizData={quizData.results} />
             <img
                 className={`blue-blob ${frontPageActive ? "blue-blob-front-page" : "blue-blob-active-game"}`}
                 src="./images/blob-blue.png"
