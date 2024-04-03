619c03b5acbcdd9d6415b79f05c689a062e58a1d
Jeremy Yochum <jyochum391@yahoo.com>
2024-04-03 10:12:46 -0400

    update classname for button

---
+++ components/FrontPage.jsx
@@ -0,0 +1,12 @@
+
+export default function FrontPage() {
+
+
+    return (
+        <div className="front-page-wrapper">
+            <h1>Quizzical</h1>
+            <h2>Test your knowledge.</h2>
+            <button className="start-button">Start Quiz</button>
+        </div>
+    )
+}
