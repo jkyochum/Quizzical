4e5e25906a55e02695fc1593cc68be10b1ee0853
Jeremy Yochum <jyochum391@yahoo.com>
2024-04-03 11:11:19 -0400

    add state for front page / create toggle function to hide FrontPage

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,18 @@
+import React, { useState, useEffect } from "react"
 import "../sass/styles.scss"
 import FrontPage from "./FrontPage"
 
 export default function App() {
+    const [frontPageActive, setFrontPageActive] = useState(true)
+
+    function toggleFrontPage() {
+        setFrontPageActive(previousValue => !previousValue)
+    }
 
     return (
         <div>
             <img className="yellow-blob" src="./images/blob-yellow.png" alt="yellow blob" />
-            <FrontPage />
+            {frontPageActive && <FrontPage toggleFrontPage={toggleFrontPage} />}
             <img className="blue-blob" src="./images/blob-blue.png" alt="blue blob" />
         </div>
     )
