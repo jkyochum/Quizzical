1ff9a4d545fa9ab3516ae41c708a41a0f13d45a0
Jeremy Yochum <jyochum391@yahoo.com>
2024-04-03 11:11:48 -0400

    pass toggle function as prop and place on button click

--- components/FrontPage.jsx
+++ components/FrontPage.jsx
@@ -1,12 +1,17 @@
 
-export default function FrontPage() {
+export default function FrontPage(props) {
 
 
     return (
         <div className="front-page-wrapper">
             <h1 className="front-page-title">Quizzical</h1>
             <h2 className="front-page-description">Test your knowledge.</h2>
-            <button className="control-button">Start quiz</button>
-        </div>
+            <button
+                className="control-button"
+                onClick={props.toggleFrontPage}
+            >
+                Start quiz
+            </button>
+        </div >
     )
 }
